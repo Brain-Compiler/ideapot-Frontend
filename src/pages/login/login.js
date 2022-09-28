c261abdeffea154b9d0b0c84c5ed484553b6ce62
limjh5699 <limjh5699@gmail.com>
2022-09-29 04:03:40 +0900

    Add Login Page

---
+++ src/pages/login/login.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import document from '../../assets/document.png'
+import styles from './login.module.scss'
+import { Link } from "react-router-dom"
+
+const Login = () => {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.main}>
+        <div className={styles.title}>
+          <h1><img className={styles.documentImage} alt='document' src={document} />IdeaPot</h1>
+        </div>
+        <div className={styles.inputBox}>
+          <div className={styles.idInput}>
+            <p>아이디</p>
+            <input type='text' onChange={e => setId(e.target.value)} />
+          </div>
+          <div className={styles.passwordInput}>
+            <p>비밀번호</p>
+            <input type='text' onChange={e => setPassword(e.target.value)} />
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <input type='button' value='로그인' />
+          <Link to="/register"><p>회원가입</p></Link>
+          <Link to="/"><p>아이디 찾기</p></Link>
+          <Link to="/"><p>비밀번호 찾기</p></Link>
+        </div>
+      </div>
+      <div className={styles.login}>
+        <p>Login</p>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/utils/router.js
+++ src/utils/router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Register from "../pages/register/register.js";
+import Register from "../pages/register/register";
+import Login from '../pages/login/login'
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Register />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
