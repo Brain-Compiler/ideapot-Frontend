1d3c78fe561a3b3fd202e53f1ebabaa7a81da4a0
limjh5699 <limjh5699@gmail.com>
2022-10-11 23:30:41 +0900

    Add Second Password Reset Page

--- src/pages/reset_pw/reset_pw.js
+++ src/pages/reset_pw/reset_pw.js
@@ -8,6 +8,8 @@ import { Link } from "react-router-dom";
 const ResetPw = () => {
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [checkNewPassword, setCheckNewPassword] = useState("");
   const [isFind, setIsFind] = useState(false);
 
   return (
@@ -16,23 +18,42 @@ const ResetPw = () => {
         <div className={styles.title}>
           <h1><img className={styles.documentImage} alt="document" src={document} />IdeaPot</h1>
         </div>
-        <div className={styles.inputBox}>
-          <div className={styles.emailInput}>
-            <p>이메일</p>
-            <input type="text" value={email} onChange={e => setEmail(e.target.value)} /> <label htmlFor="sendEmail"><img className={styles.sendEmailImage} alt="plane" src={plane} /></label>
-            <input type='button' id='sendEmail' className={styles.checkButton} />
+        { !isFind ? (
+          <div className={styles.inputBox}>
+            <div className={styles.emailInput}>
+              <p>이메일</p>
+              <input type="text" value={email} onChange={e => setEmail(e.target.value)} /> <label htmlFor="sendEmail"><img className={styles.sendEmailImage} alt="plane" src={plane} /></label>
+              <input type='button' id='sendEmail' className={styles.checkButton} />
+            </div>
+            <div className={styles.codeInput}>
+              <p>비밀번호</p>
+              <input type="text" value={code} onChange={e => setCode(e.target.value)} /> <label htmlFor="codeCheck"><img className={styles.checkCodeImage} alt="checkMark" src={checkMark} /></label>
+              <input type='button' id='codeCheck' className={styles.checkButton} />
+            </div>
           </div>
-          <div className={styles.codeInput}>
-            <p>비밀번호</p>
-            <input type="text" value={code} onChange={e => setCode(e.target.value)} /> <label htmlFor="codeCheck"><img className={styles.checkCodeImage} alt="checkMark" src={checkMark} /></label>
-            <input type='button' id='codeCheck' className={styles.checkButton} />
+        ) : (
+          <div className={styles.inputBox}>
+            <div className={styles.newPasswordInput}>
+              <p>새 비밀번호</p>
+              <input type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} />
+            </div>
+            <div className={styles.checkNewPasswordInput}>
+              <p>새 비밀번호 확인</p>
+              <input type="password" value={checkNewPassword} onChange={e => setCheckNewPassword(e.target.value)} />
+            </div>
           </div>
-        </div>
-        <div className={styles.bottom}>
-          <input type="button" value="다음" />
-          <Link to="/login"><p>로그인</p></Link>
-          <Link to="/searchId"><p>아이디 찾기</p></Link>
-        </div>
+        )}
+        { !isFind ? (
+          <div className={styles.bottom}>
+            <input type="button" value="다음" />
+            <Link to="/login"><p>로그인</p></Link>
+            <Link to="/searchId"><p>아이디 찾기</p></Link>
+          </div>
+        ) : (
+          <div className={styles.bottom}>
+            <input type="button" value="변경" />
+          </div>
+        )}
       </div>
       <div className={styles.password}>
         <p>Reset Your Password</p>
