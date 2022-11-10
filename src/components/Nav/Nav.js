dae1840979677041e44572306aceca3676a4642f
limjh5699 <limjh5699@gmail.com>
2022-11-10 19:50:10 +0900

    Edit Nav Styles

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import styles from "./Nav.module.scss";
-import { FaBars, FaCaretDown } from "react-icons/fa"
+import { FaBars, FaCaretDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className={styles.box}>
+    <div className={styles.wrap}>
+      <div className={styles.box}>
         <div className={styles.category}>
           <FaBars /> <p>카테고리</p> <FaCaretDown />
         </div>
@@ -14,8 +15,9 @@ const Nav = () => {
           <Link>고객센터</Link>
         </div>
         <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Nav;
