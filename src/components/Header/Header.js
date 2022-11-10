50726407312387cdd3125f5577ec3eefd41678e8
limjh5699 <limjh5699@gmail.com>
2022-11-10 19:45:07 +0900

    Add Base Width for Global css and Edit Header Style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,43 +2,61 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 import logo from "../../assets/logo_icon.png";
-import search from "../../assets/search_icon.png"
-import write from "../../assets/write_icon.png"
-import profile from "../../assets/userIcon.png"
+import search from "../../assets/search_icon.png";
+import write from "../../assets/write_icon.png";
+import profile from "../../assets/userIcon.png";
 
 const Header = () => {
   const [searchText, setSearchText] = useState("");
 
   const onClickHanddler = () => {
     console.log("클릭됨");
-  }
+  };
 
   return (
-    <div className={styles.box}>
-      <div className={styles.title}>
-        <Link to="/">
-          <img alt="logo" src={logo} /> <span><b>IdeaPot</b></span>
-        </Link>
-      </div>
-      <div className={styles.search}>
-        <input className={styles.searchBar} placeholder="키워드를 통해 검색해보세요!" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-        <label htmlFor="search"><img src={search} alt="search" /></label>
-        <input type="button" className={styles.searchBtn} id="search" onClick={onClickHanddler} />
-      </div>
-      <div className={styles.sub}>
-        <div className={styles.write}>
+    <div className={styles.wrap}>
+      <div className={styles.box}>
+        <div className={styles.title}>
           <Link to="/">
-            <img src={write} alt="write" />등록
+            <img alt="logo" src={logo} />{" "}
+            <span>
+              <b>IdeaPot</b>
+            </span>
           </Link>
         </div>
-        <div className={styles.profile}>
-          <Link to="/">
-            <img src={profile} alt="profile" />
-          </Link>
+        <div className={styles.search}>
+          <input
+            className={styles.searchBar}
+            placeholder="키워드를 통해 검색해보세요!"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <label htmlFor="search">
+            <img src={search} alt="search" />
+          </label>
+          <input
+            type="button"
+            className={styles.searchBtn}
+            id="search"
+            onClick={onClickHanddler}
+          />
+        </div>
+        <div className={styles.sub}>
+          <div className={styles.write}>
+            <Link to="/">
+              <img src={write} alt="write" />
+              등록
+            </Link>
+          </div>
+          <div className={styles.profile}>
+            <Link to="/">
+              <img src={profile} alt="profile" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
